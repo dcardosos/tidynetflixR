6c7df1462e286ef396daa98ca4a7d1aa8dcc207e
dcardosos <douglascardoso@usp.br>
2021-11-26 03:06:49 -0300

    base de dados importada

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Base netflix do Tidytuesday
+#'
+#' Uma base com informacoes sobre o Netflix utilizada no Tidytuesday de
+#' 20/04/21
+#'
+#'
+#' @format O formato da base:
+#' \describe{
+#'   \item{coluna}{descricao}
+#' }
+#' @source \url{https://www.kaggle.com/shivamb/netflix-shows?select=netflix_titles.csv}
+"netflix"
